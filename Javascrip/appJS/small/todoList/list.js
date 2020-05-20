4b004ded126fa773b80200fd22fe20e98995e098
xuanquyet91 <41838848+xuanquyet91@users.noreply.github.com>
2020-05-20 23:42:51 +0700

    Javascrip\appJS\small\todoList

--- Javascrip/appJS/small/todoList/list.js
+++ Javascrip/appJS/small/todoList/list.js
@@ -5,12 +5,20 @@ function onAddClick() {
 	todos.push(newTodo);
 
 	updateTodo();
+
+	document.getElementById('newTodo').value = '';
 }
 
 function updateTodo() {
 	var eleTodolist = document.getElementById('todo-list');
 	eleTodolist.innerHTML = '';
 	for (var i = 0; i < todos.length; i++) {
-		eleTodolist.innerHTML += '<li>' + todos[i] + '</li>';
+		eleTodolist.innerHTML += '<li onclick="removeTodo('+ i +');">' + todos[i] + '</li>';
 	 }
 };
+
+function removeTodo(index){
+	todos.splice(index,1);
+
+	updateTodo();
+}
