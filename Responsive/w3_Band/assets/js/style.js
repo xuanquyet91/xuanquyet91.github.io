c8c0b077b6f0c0cec532ab294290bca8e34b31b8
Quyet <phamxuanquyet91@gmail.com>
2021-10-26 05:08:22 +0700

    responsive_mobile

--- Responsive/w3_Band/assets/js/style.js
+++ Responsive/w3_Band/assets/js/style.js
@@ -1,3 +1,4 @@
+// begin modal 
 const buyBtns = document.querySelectorAll('.js-buy-tickets');
 const openModal = document.querySelector('.modal')
 const closeModal = document.querySelector('.js-modal-close')
@@ -24,3 +25,36 @@ stopBubbling.addEventListener('click', function(event) {
 for (const buyBtn of buyBtns) {
     buyBtn.addEventListener('click', showBuyTickets)
 }
+// end modal 
+
+// begin nav
+const headerNav = document.querySelector('#header');
+const menuMobile = document.querySelector('.mobile-menu-btn');
+var currentHeight = headerNav.clientHeight;
+
+// Đóng mở menu mobile
+menuMobile.onclick = function() {
+        if (headerNav.clientHeight === currentHeight) {
+            console.log('a');
+            return headerNav.style.height = 'auto';
+        } else {
+            console.log('b');
+            return headerNav.style.height = null;
+        }
+    }
+    //-------- Tự động đóng khi click chọn mennu
+const menuItems = document.querySelectorAll('#header li a[href*="#"]');
+
+for (let i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    console.log(menuItem.nextElementSibling)
+    menuItem.onclick = function(event) {
+        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if (isParentMenu) {
+            event.preventDefault();
+
+        } else {
+            headerNav.style.height = null;
+        }
+    }
+};
